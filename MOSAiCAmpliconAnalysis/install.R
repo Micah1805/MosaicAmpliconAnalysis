2cb9cefcd75b097aa435177fb38398f239ba1185
Micah1805 <93141851+Micah1805@users.noreply.github.com>
2025-09-18 12:12:56 +0200

    Update install.R

--- MOSAiCAmpliconAnalysis/install.R
+++ MOSAiCAmpliconAnalysis/install.R
@@ -1,8 +1,9 @@
 # install.R — installs required packages for MosaicAmpliconAnalysis
 
-install.packages(c("tidyverse", "phyloseq", "vegan", "data.table", "ggplot2", "ape"))
+install.packages(c("tidyverse", "dplyr", "plyr, "tydyr, "vegan", "data.table", "ggplot2", "ape", "readxl", "xlsx", "stringr", "grid", "magrittr", "paletteer", "RColorBrewer", "zoo", "microeco"))
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-BiocManager::install(c("microbiome","dada2"))
+BiocManager::install(c("microbiome","dada2","Phyloseq", "Biostrings","shortRead"))
+
